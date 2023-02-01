bd0719ed7996a70df64b84751124040be419b236
BellaBradbury <bellabradbury@yahoo.com>
2023-02-01 03:23:29 -0500

    auth-user built

--- auth-user.js
+++ auth-user.js
@@ -1,6 +1,43 @@
 'use strict';
 
-exports.authenticateUser = async (req, res, next) => {
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const models = require('./models');
+const { User } = models;
 
-    next();
+exports.authenticateUser = async (req, res, next) => {
+    let message;
+
+    // parse the user's credentials
+    const credentials = auth(req);
+
+    if(credentials) {
+        // checks to see if a username matches and retrieves it
+        const user = await User.findOne({ where: {username: credentials.emailAddress }});
+        
+        if(user) {
+            // compares the given password with the known password
+            const authenticated = bcrypt
+                .compareSync(credentials.pass, user.password);
+                if(authenticated) {
+                    console.log('Sussful login attempt');
+
+                    // stores the user
+                    req.currentUser = user;
+                } else {
+                    message = "Incorrect password";
+                }
+        } else {
+            message = "Account not found";
+        }
+    } else {
+        message = "No method of authentication found"
+    }
+
+    if(message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+    } else {
+        next();
+    }
 }
